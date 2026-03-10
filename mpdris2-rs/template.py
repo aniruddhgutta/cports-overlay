2248b47b408ce152b2b9bae9851adc145a5a3e50
aniruddhgutta <aniruddhgutta@disroot.org>
2026-03-10 09:51:52 +0530

    user/mpdris2-rs: update to 1.1.0

--- mpdris2-rs/template.py
+++ mpdris2-rs/template.py
@@ -1,5 +1,5 @@
 pkgname = "mpdris2-rs"
-pkgver = "1.0.2"
+pkgver = "1.1.0"
 pkgrel = 0
 build_style = "cargo"
 hostmakedepends = ["cargo-auditable"]
@@ -13,7 +13,7 @@ pkgdesc = "Exposing MPRIS V2.2 D-Bus interface for mpd"
 license = "GPL-3.0-or-later"
 url = "https://github.com/szclsya/mpdris2-rs"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "465d5662df14156eb9994d1fc88d51144b14bcf568f4f7cebd930a591bf7840b"
+sha256 = "16d754c87aeadec8ab29e91eb42f2bfd397e1bc0f3676784a80cbefecd7f9475"
 hardening = ["vis", "cfi"]
 
 
